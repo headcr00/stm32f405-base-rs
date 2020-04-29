7117533af28e5bbfe6dac1bf2d16c10d8946176a
Andrei Schwabenland <headcrab.0000@gmail.com>
2020-04-29 11:57:58 +0500

    Initial commit
    Schau mal, diese Programm sollt ausgefuehren, aber nicht stimmt.

---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+
+This project is built on top of `cortex-m-quickstart`.
+Test firmware, uses `cmim` to share variables across interrupts and main program.
+
+The easiest way to debug this program is to use SEGGER Ozone and J-Link.
---
+++ src/main.rs
@@ -0,0 +1,106 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// extern crate panic_abort; // requires nightly
+// extern crate panic_itm; // logs messages over ITM; requires ITM support
+// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+
+use cortex_m::asm;
+use cortex_m_rt::entry;
+use cortex_m::peripheral::{syst};
+use stm32f4::{
+    stm32f405
+};
+
+use stm32f405::interrupt;
+use stm32f405::Peripherals;
+use cortex_m_rt::exception;
+use cmim::{
+    Move,
+    Context,
+    Exception,
+};
+use cmim::Context::Interrupt;
+use core::borrow::BorrowMut;
+
+
+struct SharedData<'a>{
+    something: u8,
+    timer_data: &'a mut Peripherals,
+}
+static SHARED_DATA: Move<SharedData, stm32f4::stm32f405::Interrupt> = Move::new_uninitialized(Interrupt(stm32f405::Interrupt::TIM2));
+
+#[entry]
+fn main() -> ! {
+    let peripherals = cortex_m::peripheral::Peripherals::take().unwrap();
+    let mut systick = peripherals.SYST;
+
+    systick.set_clock_source(syst::SystClkSource::Core);
+    systick.set_reload(1_000);
+
+    systick.clear_current();
+    systick.enable_counter();
+    systick.enable_interrupt();
+
+    let mut stm_peripherals = stm32f405::Peripherals::take().unwrap();
+    SHARED_DATA.try_lock(|d| d.timer_data = stm_peripherals.borrow_mut()).ok();
+    tim2_setup(&mut stm_peripherals);
+    while !systick.has_wrapped() {
+    }
+    stm_peripherals.RCC.ahb1enr.write(|w| w.gpioden().bit(true));
+    stm_peripherals.GPIOD.moder.write(|w| w.moder12().bits(1));
+
+    loop {
+        // your code goes here
+         if let Some(value) = SHARED_DATA.try_lock(|data| data.something).ok()
+         {
+             if value > 64
+             {
+                 stm_peripherals.GPIOD.odr.write(|w| w.odr12().bit(true));
+             }
+             else {
+                 stm_peripherals.GPIOD.odr.write(|w| w.odr12().bit(false));
+             }
+         }
+    }
+}
+
+fn tim2_setup(per : &mut Peripherals)
+{
+    per.RCC.apb1rstr.write(|d| d.tim2rst().set_bit());
+    per.RCC.apb1rstr.write(|d| d.tim2rst().clear_bit());
+
+    per.RCC.apb1enr.write(|d| d.tim2en().set_bit());
+
+    per.TIM2.dier.write(|d| d.uie().set_bit());
+    per.TIM2.arr.write(|d|d.arr().bits(8000));
+    per.TIM2.psc.write(|d| d.psc().bits(10));
+
+    per.TIM2.cr1.write(|d|d.cen().set_bit());
+    unsafe{
+        cortex_m::peripheral::NVIC::unmask(stm32f405::interrupt::TIM2);
+    }
+
+}
+#[interrupt]
+fn TIM2()
+{
+    SHARED_DATA.try_lock(|mut data| {
+        data.timer_data.TIM2.sr.write(|d| d.uif().clear_bit());
+        if data.something < 128
+        {
+            data.something = data.something + 1;
+        }
+        else {
+            data.something = 0;
+        }
+    }).ok();
+}
+#[exception]
+fn SysTick()
+{
+    static mut COUNT: u32 = 0;
+    *COUNT += 1;
+}
+
