bc6fa889bec466a0c2e3309fd4fd934bbffee495
Andrei Schwabenland <headcrab.0000@gmail.com>
2020-05-08 11:18:02 +0500

    Teilen Peripherie mit dem Mutex
    Jetzt habe ich einen guten Beispiel fur Peripheriegerateteilung!
    Mutex macht alle Interrupten ausschalten, und kluge Leute denken das
    macht Ihre App fehlerfrei.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![no_std]
 #![no_main]
-use panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // extern crate panic_abort; // requires nightly
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
@@ -10,19 +10,17 @@ use cortex_m::peripheral::{syst};
 use stm32l4::{
     stm32l4x6
 };
+use shared_cell::SharedCell;
 
 use stm32l4x6::interrupt;
 
 use stm32l4x6::Peripherals;
 use cortex_m_rt::exception;
-use core::borrow::BorrowMut;
+use cortex_m::interrupt::Mutex;
 
 static MY_SHARED_VAR: shared_cell::SharedCell<SharedData> = shared_cell::SharedCell::uninit();
-static MY_SHARED_PER: shared_cell::SharedCell<PeripheralsShared> = shared_cell::SharedCell::uninit();
-struct PeripheralsShared
-{
-    holder: stm32l4x6::Peripherals,
-}
+static MY_SHARED_PER_MUTEXD: Mutex<SharedCell<stm32l4x6::Peripherals>> = Mutex::new(SharedCell::uninit());
+
 struct SharedData {
     something: u16,
 }
@@ -30,11 +28,10 @@ struct SharedData {
 fn main() -> ! {
     let peripherals = cortex_m::peripheral::Peripherals::take().unwrap();
     let mut systick = peripherals.SYST;
-    let mut stm_peripherals = stm32l4x6::Peripherals::take().unwrap();
+    let  stm_peripherals = stm32l4x6::Peripherals::take().unwrap();
 
-    MY_SHARED_PER.initialize(
-        PeripheralsShared{holder: stm_peripherals}
-    );
+    cortex_m::interrupt::free(|cs| MY_SHARED_PER_MUTEXD.borrow(cs).initialize(stm_peripherals));
+
     MY_SHARED_VAR.initialize(SharedData{
         something : 0,
     });
@@ -49,30 +46,33 @@ fn main() -> ! {
     //tim2_setup(&mut stm_peripherals);
     while !systick.has_wrapped() {
     }
-    
-    if let Some(pers) = MY_SHARED_PER.get_value()
-    {
-        pers.holder.RCC.ahb2enr.write(|w| w.gpioben().set_bit());
-        pers.holder.GPIOB.moder.write(|w| w.moder2().output());
-        tim2_setup(&pers.holder);
-    }
+    cortex_m::interrupt::free(|cs|{
+        if let Some(pers) = MY_SHARED_PER_MUTEXD.borrow(cs).get_value()
+        {
+            pers.RCC.ahb2enr.write(|w| w.gpioben().set_bit());
+            pers.GPIOB.moder.write(|w| w.moder2().output());
+            tim2_setup(&pers);
+        }
+    });
+
 
 
 
     loop {
         // your code goes here
          if let Some(value) = MY_SHARED_VAR.get_value().as_ref(){
-             if let Some(pers) = MY_SHARED_PER.get_value()
-             {
-                 if value.something > 250
-                 {
-                     pers.holder.GPIOB.odr.write(|w| w.odr2().set_bit());
-                 } else {
-                     pers.holder.GPIOB.odr.write(|w| w.odr2().clear_bit());
-                 }
-             }
-
-         }
+            cortex_m::interrupt::free( |cs| {
+                if let Some(pers) = MY_SHARED_PER_MUTEXD.borrow(cs).get_value()
+                {
+                    if value.something > 250
+                    {
+                    pers.GPIOB.odr.write(|w| w.odr2().set_bit());
+                    } else {
+                    pers.GPIOB.odr.write(|w| w.odr2().clear_bit());
+                    }
+                }
+            });
+        }
     }
 }
 
@@ -93,17 +93,20 @@ fn tim2_setup(per : &Peripherals)
 #[interrupt]
 fn TIM2()
 {
-    if let Some(p) = MY_SHARED_PER.get_value()
-    {
-        p.holder.TIM2.sr.write(|d| d.uif().clear_bit());
-        MY_SHARED_VAR.modify(|d| {
-            d.something = d.something + 1;
-            if d.something == 750
-            {
-                d.something = 0;
-            }
-        }).ok();
-    }
+    cortex_m::interrupt::free( |cs| {
+        if let Some(p) = MY_SHARED_PER_MUTEXD.borrow(cs).get_value()
+        {
+            p.TIM2.sr.write(|d| d.uif().clear_bit());
+            MY_SHARED_VAR.modify(|d| {
+                d.something = d.something + 1;
+                if d.something == 750
+                {
+                    d.something = 0;
+                }
+            }).ok();
+        }
+    });
+
 
 }
 #[exception]
