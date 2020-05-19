fe37e7f735ad54c52a683790a71998e387a289ed
Andrei Schwabenland <headcrab.0000@gmail.com>
2020-05-19 11:06:31 +0500

    Datenaustausch mit DMA und UART
    Das code ist nich so schoen jetzt, aber funktionell.
    Das CPU bekommt Daten mit UART2 in DMA Buffer von 32 Bytes, dann schickt
    alles zuruck.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
 #![no_std]
 #![no_main]
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+                     // extern crate panic_abort; // requires nightly
+                     // extern crate panic_itm; // logs messages over ITM; requires ITM support
+                     // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 mod shared_cell;
 mod uart_dma;
+use cortex_m::peripheral::syst;
 use cortex_m_rt::entry;
-use cortex_m::peripheral::{syst};
-use stm32l4::{
-    stm32l4x6
-};
 use shared_cell::SharedCell;
-use uart_dma::UartDma;
+use stm32l4::stm32l4x6;
 use stm32l4x6::interrupt;
+use uart_dma::UartDmaWrapper;
 
-use stm32l4x6::Peripherals;
-use cortex_m_rt::exception;
 use cortex_m::interrupt::Mutex;
+use cortex_m_rt::exception;
+use stm32l4x6::Peripherals;
 
-static MY_SHARED_VAR_MUTEXD: Mutex<shared_cell::SharedCell<SharedData>> = Mutex::new(shared_cell::SharedCell::uninit());
+static MY_SHARED_VAR_MUTEXD: Mutex<shared_cell::SharedCell<SharedData>> =
+    Mutex::new(shared_cell::SharedCell::uninit());
 static MY_SHARED_PER: SharedCell<stm32l4x6::Peripherals> = SharedCell::uninit();
-
 
 struct SharedData {
     something: u16,
@@ -30,10 +28,14 @@ struct SharedData {
 fn main() -> ! {
     let peripherals = cortex_m::peripheral::Peripherals::take().unwrap();
     let mut systick = peripherals.SYST;
-    let  stm_peripherals = stm32l4x6::Peripherals::take().unwrap();
+    let stm_peripherals = stm32l4x6::Peripherals::take().unwrap();
 
     MY_SHARED_PER.initialize(stm_peripherals);
-    cortex_m::interrupt::free(|cs| MY_SHARED_VAR_MUTEXD.borrow(cs).initialize(SharedData{something : 0}));
+    cortex_m::interrupt::free(|cs| {
+        MY_SHARED_VAR_MUTEXD
+            .borrow(cs)
+            .initialize(SharedData { something: 0 })
+    });
 
     systick.set_clock_source(syst::SystClkSource::Core);
     systick.set_reload(1_000);
@@ -42,76 +44,74 @@ fn main() -> ! {
     systick.enable_counter();
     systick.enable_interrupt();
 
-    let uart = UartDma::init().unwrap();
-    //tim2_setup(&mut stm_peripherals);
-    while !systick.has_wrapped() {
-    }
-        if let Some(pers) = MY_SHARED_PER.get_value()
-        {
-            pers.RCC.ahb2enr.write(|w| w.gpioben().set_bit());
-            pers.GPIOB.moder.write(|w| w.moder2().output());
-            tim2_setup(&pers);
-        }
+    let uart = UartDmaWrapper::init().unwrap();
+    while !systick.has_wrapped() {}
+    if let Some(pers) = MY_SHARED_PER.get_value()
+    ////////
+    {
+        pers.RCC.ahb2enr.modify(|_, w| w.gpioben().set_bit());
+        pers.RCC.ahb2enr.modify(|_, w| w.gpioeen().set_bit());
 
+        pers.GPIOB.moder.write(|w| w.moder2().output());
+        pers.GPIOE.moder.write(|w| w.moder8().output());
+        pers.GPIOB.pupdr.write(|w| w.pupdr2().pull_up());
+        pers.GPIOE.pupdr.write(|w| w.pupdr8().pull_up());
+
+        pers.GPIOB.otyper.write(|r| r.ot2().push_pull());
+        pers.GPIOE.odr.write(|w| w.odr8().set_bit());
+
+        tim2_setup(&pers);
+    }
     loop {
         // your code goes here
         cortex_m::interrupt::free(|cs| {
-            if let Some(value) = MY_SHARED_VAR_MUTEXD.borrow(cs).get_value()
-            {
-                if let Some(pers) = MY_SHARED_PER.get_value()
-                {
-                   if value.something > 250
-                    {
-                    pers.GPIOB.odr.write(|w| w.odr2().set_bit());
+            if let Some(value) = MY_SHARED_VAR_MUTEXD.borrow(cs).get_value() {
+                if let Some(pers) = MY_SHARED_PER.get_value() {
+                    if value.something > pers.DMA1.cndtr6.read().ndt().bits() * 10 {
+                        pers.GPIOE.odr.modify(|_, w| w.odr8().clear_bit());
                     } else {
-                    pers.GPIOB.odr.write(|w| w.odr2().clear_bit());
+                        pers.GPIOE.odr.modify(|_, w| w.odr8().set_bit());
                     }
+                    //                    if uart.do_something() == true {
+                    //                        pers.GPIOB.odr.modify(|_, w| w.odr2().set_bit());
+                    //                    } else {
+                    //                        pers.GPIOB.odr.modify(|_, w| w.odr2().clear_bit());
+                    //                    }
                 }
             }
-        }
-
-            uart.do_something()
-        );
-
+        });
     }
 }
 
-fn tim2_setup(per : &Peripherals)
-{
-
-    per.RCC.apb1enr1.write(|d| d.tim2en().set_bit());
+fn tim2_setup(per: &Peripherals) {
+    per.RCC.apb1enr1.modify(|_, w| w.tim2en().set_bit());
     per.TIM2.dier.write(|d| d.uie().set_bit());
-    per.TIM2.arr.write(|d|d.arr().bits(100));
+    per.TIM2.arr.write(|d| d.arr().bits(400));
     per.TIM2.psc.write(|d| d.psc().bits(10));
-
-    per.TIM2.cr1.write(|d|d.cen().set_bit());
-    unsafe{
+    per.TIM2.cr1.write(|d| d.cen().set_bit());
+    unsafe {
         cortex_m::peripheral::NVIC::unmask(stm32l4x6::interrupt::TIM2);
     }
-
 }
 #[interrupt]
-fn TIM2()
-{
-    cortex_m::interrupt::free( |cs| {
-        if let Some(p) = MY_SHARED_PER.get_value()
-        {
+fn TIM2() {
+    cortex_m::interrupt::free(|cs| {
+        if let Some(p) = MY_SHARED_PER.get_value() {
             p.TIM2.sr.write(|d| d.uif().clear_bit());
-            MY_SHARED_VAR_MUTEXD.borrow(cs).modify(|d| {
-                d.something = d.something + 1;
-                if d.something == 750
-                {
-                    d.something = 0;
-                }
-            }).ok();
+            MY_SHARED_VAR_MUTEXD
+                .borrow(cs)
+                .modify(|d| {
+                    d.something = d.something + 1;
+                    if d.something == 1000 {
+                        d.something = 0;
+                    }
+                })
+                .ok();
         }
     });
-
-
 }
 #[exception]
-fn SysTick()
-{
+fn SysTick() {
     // MY_SHARED_VAR.modify(|d| {
     //     d.something = d.something + 1;
     //     if d.something == 750
--- src/uart_dma.rs
+++ src/uart_dma.rs
@@ -1,65 +1,146 @@
-use cortex_m::interrupt::Mutex;
-use crate::stm32l4x6::interrupt;
 use crate::shared_cell::SharedCell;
+use crate::stm32l4x6::interrupt;
 use crate::MY_SHARED_PER;
+use core::borrow::Borrow;
+use cortex_m::interrupt::Mutex;
+use stm32l4::stm32l4x6::usart1::RegisterBlock;
+
 //PD5 PD6
-static MY_SHARED_UART : Mutex<SharedCell<UartDma>> = Mutex::new(SharedCell::uninit());
-static mut SINGLE_BORROW : bool = false;
-struct UartDma
-{
-    buf: [u8; 128],
+static MY_SHARED_UART: Mutex<SharedCell<UartDma>> = Mutex::new(SharedCell::uninit());
+static mut SINGLE_BORROW: bool = false;
+struct UartDma {
+    buf: [u8; 32],
+    pos: usize,
 }
 
-pub struct UartDmaWrapper
-{
-}
+pub struct UartDmaWrapper {}
 
 impl UartDmaWrapper {
-    pub fn init() -> Option<Self>
-    {
-        if unsafe{SINGLE_BORROW == false}
-        {
-            unsafe{SINGLE_BORROW = true};
-            cortex_m::interrupt::free(|cs| MY_SHARED_UART.borrow(cs).initialize(
-                UartDma{
-                    buf: [0; 128]
-                };
-            ));
-            if let Some(per) = MY_SHARED_PER.get_value()
-            {
+    pub fn init() -> Option<Self> {
+        if unsafe { SINGLE_BORROW == false } {
+            unsafe { SINGLE_BORROW = true };
+            cortex_m::interrupt::free(|cs| {
+                MY_SHARED_UART.borrow(cs).initialize(UartDma {
+                    buf: [0; 32],
+                    pos: 0,
+                });
+            });
+            if let Some(p) = MY_SHARED_PER.get_value() {
+                p.RCC.ahb2enr.modify(|_, w| w.gpioden().set_bit());
+                p.RCC.apb1enr1.modify(|_, w| w.usart2en().set_bit());
+                p.GPIOD.pupdr.modify(|_, w| {
+                    w.pupdr5().pull_up();
+                    w.pupdr6().pull_up()
+                });
 
-                per.DMA1.cpar1.write(|addr| unsafe {addr.bits(my.buf.as_ptr() as usize as u32)});
+                p.GPIOD.moder.modify(|_, w| {
+                    w.moder5().alternate();
+                    w.moder6().alternate()
+                });
+                p.GPIOD.ospeedr.modify(|_, w| {
+                    w.ospeedr5().very_high_speed();
+                    w.ospeedr6().very_high_speed()
+                });
+
+                p.GPIOD.otyper.modify(|_, w| {
+                    w.ot5().push_pull();
+                    w.ot6().push_pull()
+                });
+
+                p.GPIOD.afrl.modify(|_, w| {
+                    w.afrl5().af7();
+                    w.afrl6().af7()
+                });
+
+                UartDmaWrapper::init_usart2(&p.USART2, 9600);
+                UartDmaWrapper::init_usart_dma(&p);
             }
-            Some(my)
-        }
-        else {
+            Some(UartDmaWrapper {})
+        } else {
             None
         }
     }
 
-    fn init_usart2(uart : &crate::stm32l4x6::USART2)
-    {
-        uart.cr1.write(|d| d.);
-
+    fn init_usart2(uart: &crate::stm32l4x6::USART2, speed: u32) {
+        let brr = 4_000_000 / (speed);
+        uart.brr.write(|d| d.brr().bits(brr as u16));
+        uart.cr3.modify(|_, w| w.dmar().enabled());
+        uart.cr1.write(|d| {
+            d.m0().bit8();
+            d.re().enabled();
+            d.te().enabled();
+            d.ue().enabled()
+        });
     }
 
-    pub fn do_something(&self)
-    {
-
+    fn init_usart_dma(p: &crate::stm32l4x6::Peripherals) {
+        p.RCC.ahb1enr.modify(|_, w| w.dma1en().set_bit());
+        p.DMA1
+            .cpar6
+            .write(|w| w.pa().bits(&p.USART2.rdr as *const _ as u32));
+        cortex_m::interrupt::free(|cs| {
+            MY_SHARED_UART.borrow(cs).modify(|uart_s| {
+                p.DMA1
+                    .cmar6
+                    .write(|w| w.ma().bits(&uart_s.buf as *const u8 as u32));
+                p.DMA1
+                    .cndtr6
+                    .write(|w| w.ndt().bits(unsafe { uart_s.buf.len() as u16 }));
+            });
+        });
+        p.DMA1.cselr.write(|w| w.c6s().map2());
+        p.DMA1.ccr6.write(|w| {
+            w.circ().disabled();
+            w.dir().from_peripheral();
+            w.minc().enabled();
+            w.pinc().disabled();
+            w.msize().bits8();
+            w.tcie().enabled()
+        });
+        p.DMA1.ccr6.modify(|_, w| w.en().enabled());
+        unsafe {
+            cortex_m::peripheral::NVIC::unmask(crate::stm32l4x6::interrupt::DMA1_CH6);
+        }
     }
 
+    pub fn do_something(&self) -> bool {
+        if let Some(p) = MY_SHARED_PER.get_value() {
+            if p.USART2.isr.read().rxne().bit_is_set() == true {
+                cortex_m::interrupt::free(|cs| {
+                    MY_SHARED_UART
+                        .borrow(cs)
+                        .modify(|buf| {
+                            let rchar = (p.USART2.rdr.read().bits() & 0xFF) as u8;
+                            if buf.pos >= buf.buf.len() {
+                                buf.pos = 0;
+                            }
+                            buf.buf[buf.pos] = rchar;
+                            buf.pos = buf.pos + 1;
+                        })
+                        .ok()
+                });
+                return true;
+            }
+        }
+        false
+    }
 }
 
 #[interrupt]
-fn DMA1_CH6()
-{
+fn DMA1_CH6() {
     /* USART 2 Rx handler */
-    if let Some(per) = MY_SHARED_PER.get_value()
-    {
-        if per.DMA1.isr.read().tcif6().bit_is_set()
-        {
+    if let Some(per) = MY_SHARED_PER.get_value() {
+        if per.DMA1.isr.read().tcif6().bit_is_set() {
             per.DMA1.ifcr.write(|d| d.ctcif6().set_bit());
+            cortex_m::interrupt::free(|cs| {
+                if let Some(data) = MY_SHARED_UART.borrow(cs).get_value() {
+                    for i in data.buf.iter() {
+                        while per.USART2.isr.read().txe().bit_is_clear() {}
+                        per.USART2.tdr.write(|f| unsafe { f.tdr().bits(*i as u16) });
+                    }
+                }
+            });
+            per.GPIOB.odr.modify(|_, w| w.odr2().set_bit());
         }
-
     }
 }
